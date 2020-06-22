056efad1355547600d470d580bbb001d8531315f
Marie Otaki <otakimarie@gmail.com>
2020-06-21 21:12:47 -0700

    modify files so that post data and display it

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http'),
   fs = require('fs'),
-  ejs = require('ejs');
-qs = require('querystring');
+  ejs = require('ejs'),
+  qs = require('querystring');
 
 const settings = require('./settings');
 const server = http.createServer();
@@ -10,7 +10,7 @@ const template = fs.readFileSync(__dirname + '/public_html/bbs.ejs', 'utf-8');
 const posts = [];
 function renderForm(posts, res) {
   const data = ejs.render(template, {
-    post: posts
+    posts: posts
   });
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write(data);
@@ -20,10 +20,18 @@ server.on('request', function(req, res) {
   //check if the content was post
   if (req.method === 'POST') {
     req.data = '';
-    req.on('readable', function() {
-      req.data += read.read();
+    req.on('data', function(chunk) {
+      req.data += chunk;
     });
-    req.on('end', function() {});
+    // req.on('readable', function() {
+    //   req.data += req.read();
+    //   console.log(req.data);
+    // });
+    req.on('end', function() {
+      const query = qs.parse(req.data);
+      posts.push(query.name);
+      renderForm(posts, res);
+    });
   } else {
     renderForm(posts, res);
   }
