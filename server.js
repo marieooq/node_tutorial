6682858c040ebe4847b2eb1f42ba493fa1f42ac1
Marie Otaki <otakimarie@gmail.com>
2020-06-21 17:48:30 -0700

    add an ejs file and read it in server.js

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-const http = require('http');
-const fs = require('fs');
+const http = require('http'),
+  fs = require('fs'),
+  ejs = require('ejs');
+
 const settings = require('./settings');
 const server = http.createServer();
-let msg;
+//blocking
+const template = fs.readFileSync(__dirname + '/public_html/hello.ejs', 'utf-8');
+let n = 0;
 server.on('request', function(req, res) {
-  fs.readFile(__dirname + '/public_html/hello.html', 'utf-8', function(
-    err,
-    data
-  ) {
-    if (err) {
-      res.writeHead(404, { 'Content-Type': 'text/plain' });
-      res.write('not found!');
-      return res.end();
-    }
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(data);
-    res.end();
+  n++;
+  const data = ejs.render(template, {
+    title: 'hello',
+    content: '<strong>World!</strong>',
+    n: n
   });
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write(data);
+  res.end();
 });
 server.listen(settings.port, settings.host);
 console.log('server listening...');
