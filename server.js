3c1e55d94fd57026cf5bd7fe55b85e2ea11aa7f1
Marie Otaki <otakimarie@gmail.com>
2020-06-21 18:09:47 -0700

    add bbs.ejs

--- server.js
+++ server.js
@@ -1,22 +1,32 @@
 const http = require('http'),
   fs = require('fs'),
   ejs = require('ejs');
+qs = require('querystring');
 
 const settings = require('./settings');
 const server = http.createServer();
 //blocking
-const template = fs.readFileSync(__dirname + '/public_html/hello.ejs', 'utf-8');
-let n = 0;
-server.on('request', function(req, res) {
-  n++;
+const template = fs.readFileSync(__dirname + '/public_html/bbs.ejs', 'utf-8');
+const posts = [];
+function renderForm(posts, res) {
   const data = ejs.render(template, {
-    title: 'hello',
-    content: '<strong>World!</strong>',
-    n: n
+    post: posts
   });
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write(data);
   res.end();
+}
+server.on('request', function(req, res) {
+  //check if the content was post
+  if (req.method === 'POST') {
+    req.data = '';
+    req.on('readable', function() {
+      req.data += read.read();
+    });
+    req.on('end', function() {});
+  } else {
+    renderForm(posts, res);
+  }
 });
 server.listen(settings.port, settings.host);
 console.log('server listening...');
